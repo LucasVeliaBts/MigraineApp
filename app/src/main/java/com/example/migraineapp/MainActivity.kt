ed2d70cc897cffe87d10ac48c1dbee74ca176c6b
LucasVeliaBts <lucasveliabtssio@gmail.com>
2022-09-19 16:45:17 +0400

    2

---
+++ app/src/main/java/com/example/migraineapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.migraineapp
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mPickTimeBtn = findViewById<Button>(R.id.pickDateBtn)
+        val textView     = findViewById<TextView>(R.id.dateTv)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        mPickTimeBtn.setOnClickListener {
+
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in TextView
+                textView.setText("" + dayOfMonth + " " + month + ", " + year)
+            }, year, month, day)
+            dpd.show()
+
+        }
+
+    }
+}
