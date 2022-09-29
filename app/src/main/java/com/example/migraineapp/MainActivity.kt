4d0ae287f4ba7edcfbbab126efe649d3ddb35c95
LucasVeliaBts <lucasveliabtssio@gmail.com>
2022-09-29 16:42:26 +0400

    Addition des spiners

--- app/src/main/java/com/example/migraineapp/MainActivity.kt
+++ app/src/main/java/com/example/migraineapp/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.migraineapp
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val intensites = resources.getStringArray(R.array.Intensites)
         val mPickTimeBtn = findViewById<Button>(R.id.pickDateBtn)
         val textView     = findViewById<TextView>(R.id.dateTv)
 
@@ -28,7 +28,49 @@ class MainActivity : AppCompatActivity() {
             }, year, month, day)
             dpd.show()
 
+
         }
 
+
+        // access the spinner
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, intensites)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
+
+            }
+        }
+        val traitements = resources.getStringArray(R.array.Traitements)
+        val spinner2 = findViewById<Spinner>(R.id.spinner2)
+        if (spinner2 != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, traitements)
+            spinner2.adapter = adapter
+
+            spinner2.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+
+
+            }
+        }
     }
 }
